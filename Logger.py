f638f3c62e9565fd166677aa938f81b3062180b6
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2026-06-28 12:22:48 +0200

    feat: implemented a simple game loop, and added basic glider for testing

--- Logger.py
+++ Logger.py
@@ -1,13 +1,15 @@
 import logging
 
-logger = logging.getLogger('application_logger')
+logger = logging.getLogger("application_logger")
 logger.setLevel(logging.DEBUG)
 
 if not logger.handlers:
     file_handler = logging.FileHandler("logs/application-logs.log")
     file_handler.setLevel(logging.DEBUG)
 
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     file_handler.setFormatter(formatter)
 
     logger.addHandler(file_handler)
--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import time
+from Board import Board
+
+rows, columns = 16, 16
+game_board = Board(rows, columns)
+
 if __name__ == "__main__":
-    pass
+    # glider by hand for testing
+    game_board.change_cell_liveness(1, 2)
+    game_board.change_cell_liveness(2, 3)
+    game_board.change_cell_liveness(3, 1)
+    game_board.change_cell_liveness(3, 2)
+    game_board.change_cell_liveness(3, 3)
+
+    # game loop
+    generations = 20
+    for _ in range(generations):
+        game_board.draw_board()
+        game_board.step()
+        time.sleep(0.5)
