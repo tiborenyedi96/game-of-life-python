5d423ecb1dab2c88872af7008c41995797fc34c2
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2026-06-28 10:01:37 +0200

    feat: introduced logging

---
+++ Logger.py
@@ -0,0 +1,13 @@
+import logging
+
+logger = logging.getLogger('application_logger')
+logger.setLevel(logging.DEBUG)
+
+if not logger.handlers:
+    file_handler = logging.FileHandler("logs/application-logs.log")
+    file_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
